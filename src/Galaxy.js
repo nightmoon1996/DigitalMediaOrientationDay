aa14f7b7183394f3ebe95b7d72046eeb8171d532
KanameS <nk23a4@gmail.com>
2024-04-19 18:41:03 +0700

    refactor: Improve planet description box opacity transition and scale lerp in Galaxy.js

--- src/Galaxy.js
+++ src/Galaxy.js
@@ -287,6 +287,7 @@ const Galaxy = () => {
         }
 
         scaleTimeout = setTimeout(() => {
+          selectedPlanet.scale.lerp(new THREE.Vector3(1, 1, 1), 0.1);
           descriptionBox.style.opacity = "0";
         }, 3000);
       }
