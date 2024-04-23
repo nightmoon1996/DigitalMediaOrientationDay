1897938d1d43d4555bc35980b0dcea56a0221329
KanameS <nk23a4@gmail.com>
2024-04-23 21:18:34 +0700

    refactor: remove unused component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Galaxy from "./Galaxy";
-import Navbar from "./Navbar";
-
 function App() {
   return (
     <Router>
